92d56a0e38fa794f910d05f5528f0777818ab233
Nicolas Charette-Naud <nicolas@bamboosoft.ca>
2020-04-18 00:28:58 -0400

    refactor: Rename GrassGenerator to MapGenerator

--- src/KeepItAlive/Assets/Generators/MapGenerator.cs
+++ src/KeepItAlive/Assets/Generators/MapGenerator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrassGenerator : MonoBehaviour
+public class MapGenerator : MonoBehaviour
 {
     [SerializeField]
     private GameObject grassPrefab;
