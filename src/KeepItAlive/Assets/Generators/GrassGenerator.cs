6052bb5891e5f9f72d4eb69871b53a25de51287a
Nicolas Charette-Naud <nicolas@bamboosoft.ca>
2020-04-17 23:07:49 -0400

    feat: Create GrassGenerator to make floor

---
+++ src/KeepItAlive/Assets/Generators/GrassGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject grassPrefab;
+
+    void Start()
+    {
+        for (int x = 0; x < 50; x++)
+        {
+            for (int z = 0; z < 25; z++)
+            {
+                var grassCube = Instantiate(grassPrefab);
+                grassCube.transform.position = new Vector3(x, 0, z);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
