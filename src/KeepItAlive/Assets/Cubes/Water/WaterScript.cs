51e2d2149cbe7211c4eb513906d72a2a7c8c259a
Nicolas Charette-Naud <nicolas@bamboosoft.ca>
2020-04-19 01:50:42 -0400

    feat: Animate water before going to sleep ... satisfying !

---
+++ src/KeepItAlive/Assets/Cubes/Water/WaterScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScript : MonoBehaviour
+{
+    private float timePointer;
+    private float initialY;
+
+    void Start()
+    {
+        initialY = transform.localPosition.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timePointer += Time.deltaTime * 2;
+
+        var offset = Mathf.Sin(timePointer + transform.localPosition.x + transform.localPosition.z) / 4 - 0.3f;
+
+        transform.localPosition = new Vector3(transform.localPosition.x, initialY + offset, transform.localPosition.z);
+    }
+}
